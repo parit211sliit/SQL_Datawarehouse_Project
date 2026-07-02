e4e5dee993df3a584e4fcd1b47c1de0bc1477370
Parindi Weerasekara <99245111+parit211sliit@users.noreply.github.com>
2026-07-02 16:23:10 +0530

    Create product_table_quality_check.sql

---
+++ tests/silver/product_table_quality_check.sql
@@ -0,0 +1,87 @@
+SELECT *
+FROM bronze.crm_prd_info
+
+
+-- Check for NULLs or Duplicates in Primary Key
+-- Expectation: No Results
+SELECT prd_id , COUNT(*)
+FROM bronze.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*)>1 OR prd_id IS NULL
+
+-- Check for Unwanted Spaces
+-- Expectation: No Results
+
+SELECT 
+    prd_nm 
+FROM bronze.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm);
+
+-- Check for NULLs or Negative Values in Cost
+-- Expectation: No Results
+
+SELECT 
+    prd_cost 
+FROM bronze.crm_prd_info
+WHERE prd_cost <0 OR prd_cost IS NULL
+
+-- Data Standardization & Consistency
+SELECT DISTINCT 
+    prd_line 
+FROM bronze.crm_prd_info;
+-- Check for Invalid Date Orders (Start Date > End Date)
+-- Expectation: No Results
+
+SELECT 
+    * 
+FROM bronze.crm_prd_info
+WHERE prd_end_dt < prd_start_dt;
+
+SELECT prd_id,
+       prd_key,
+	   prd_nm,
+	   prd_cost,
+	   prd_line,
+	   prd_start_dt,
+	   prd_end_dt,
+	   LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS prd_end_dt_tst
+FROM bronze.crm_prd_info
+WHERE prd_key IN ('AC-HE-HL-U509-R','AC-HE-HL-U509')
+
+-- Check for NULLs or Duplicates in Primary Key
+-- Expectation: No Results
+SELECT prd_id , COUNT(*)
+FROM silver.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*)>1 OR prd_id IS NULL
+
+-- Check for Unwanted Spaces
+-- Expectation: No Results
+
+SELECT 
+    prd_nm 
+FROM silver.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm);
+
+-- Check for NULLs or Negative Values in Cost
+-- Expectation: No Results
+
+SELECT 
+    prd_cost 
+FROM silver.crm_prd_info
+WHERE prd_cost <0 OR prd_cost IS NULL
+
+-- Data Standardization & Consistency
+SELECT DISTINCT 
+    prd_line 
+FROM silver.crm_prd_info;
+-- Check for Invalid Date Orders (Start Date > End Date)
+-- Expectation: No Results
+
+SELECT 
+    * 
+FROM silver.crm_prd_info
+WHERE prd_end_dt < prd_start_dt;
+
+SELECT *
+FROM silver.crm_prd_info
